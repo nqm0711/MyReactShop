aa5cc10cfd6c2861ac6e45c5009f6a6e2fe56b7b
nqm0711 <qm0711@gmail.com>
2023-08-04 12:15:10 +0700

    toggle cart isOpen after navigate

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,32 +1,37 @@
-import {useSelector} from 'react-redux';
+import {useDispatch, useSelector} from 'react-redux';
 import {useNavigate} from 'react-router-dom';
 
 import Button from '../button/button.component';
 import CartItem from '../cart-item/cart-item.component';
-import {selectCartItems} from '../../store/cart/cart.selector';
+import {selectCartItems, selectIsCartOpen} from '../../store/cart/cart.selector';
 
 import {CartDropdownContainer, CartItems, EmptyMessage,} from './cart-dropdown.styles';
+import {setIsCartOpen} from "../../store/cart/cart.action";
 
 const CartDropdown = () => {
-  const cartItems = useSelector(selectCartItems);
-  const navigate = useNavigate();
+    const dispatch = useDispatch();
+    const isCartOpen = useSelector(selectIsCartOpen);
+    const toggleIsCartOpen = () => dispatch(setIsCartOpen(!isCartOpen));
+    const cartItems = useSelector(selectCartItems);
+    const navigate = useNavigate();
 
-  const goToCheckoutHandler = () => {
-    navigate('/checkout');
-  };
+    const goToCheckoutHandler = () => {
+        navigate('/checkout');
+        toggleIsCartOpen()
+    };
 
-  return (
-    <CartDropdownContainer>
-      <CartItems>
-        {cartItems.length ? (
-          cartItems.map((item) => <CartItem key={item.id} cartItem={item} />)
-        ) : (
-          <EmptyMessage>Your cart is empty</EmptyMessage>
-        )}
-      </CartItems>
-      <Button onClick={goToCheckoutHandler}>GO TO CHECKOUT</Button>
-    </CartDropdownContainer>
-  );
+    return (
+        <CartDropdownContainer>
+            <CartItems>
+                {cartItems.length ? (
+                    cartItems.map((item) => <CartItem key={item.id} cartItem={item} />)
+                ) : (
+                    <EmptyMessage>Your cart is empty</EmptyMessage>
+                )}
+            </CartItems>
+            <Button onClick={goToCheckoutHandler}>GO TO CHECKOUT</Button>
+        </CartDropdownContainer>
+    );
 };
 
 export default CartDropdown;
